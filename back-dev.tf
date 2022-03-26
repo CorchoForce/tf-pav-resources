d1acb7bd633877763818a20ad51c11bc15c4fe48
Bruno Dantas <bruno.d.paiva@hotmail.com>
2022-03-26 15:22:37 -0300

    Add back heroku resources

---
+++ back-dev.tf
@@ -0,0 +1,31 @@
+resource "heroku_app" "back-dev" {
+  name   = format("%s-dev", var.back_name)
+  region = "us"
+  config_vars = {
+    "EMAIL_DOMAIN"   = "gmail.com"
+    "ENV"            = "develop"
+    "FRONT_URL"      = heroku_app.front-dev.name
+    "MONGO_DB"       = "pega-a-visao"
+    "MONGO_HOSTNAME" = "gremiodb.aiybv.mongodb.net"
+    "MONGO_PORT"     = "27017"
+    "NODE_ENV"       = "develop"
+    "PRODUCTION"     = false
+    "SERVER_PORT"    = "8080"
+  }
+
+  sensitive_config_vars = {
+    "APP_KEY"        = var.app_key
+    "EMAIL_KEY"      = var.email_key
+    "EMAIL_PASSWORD" = var.email_password
+    "MONGO_PASSWORD" = var.mongo_password
+    "MONGO_USERNAME" = var.mongo_username
+  }
+}
+
+resource "heroku_build" "back-dev" {
+  app_id = heroku_app.back-dev.id
+  source {
+    url     = var.back_repository
+    version = "main"
+  }
+}
---
+++ back-prod.tf
@@ -0,0 +1,31 @@
+resource "heroku_app" "back-prod" {
+  name   = format("%s-prod", var.back_name)
+  region = "us"
+  config_vars = {
+    "EMAIL_DOMAIN"   = "gmail.com"
+    "ENV"            = "production"
+    "FRONT_URL"      = heroku_app.front-prod.name
+    "MONGO_DB"       = "pega-a-visao"
+    "MONGO_HOSTNAME" = "gremiodb.aiybv.mongodb.net"
+    "MONGO_PORT"     = "27017"
+    "NODE_ENV"       = "production"
+    "PRODUCTION"     = true
+    "SERVER_PORT"    = "8080"
+  }
+
+  sensitive_config_vars = {
+    "APP_KEY"        = var.app_key
+    "EMAIL_KEY"      = var.email_key
+    "EMAIL_PASSWORD" = var.email_password
+    "MONGO_PASSWORD" = var.mongo_password
+    "MONGO_USERNAME" = var.mongo_username
+  }
+}
+
+resource "heroku_build" "back-prod" {
+  app_id = heroku_app.back-prod.id
+  source {
+    url     = var.back_repository
+    version = "main"
+  }
+}
